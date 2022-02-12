17a0f53c8b8ac8afd4f161cd4034a2e9c345c1b0
spinute <spinutids@gmail.com>
2022-02-13 02:13:19 +0900

    add fish

---
+++ fish/config.fish
@@ -0,0 +1,56 @@
+alias l=ls
+alias la='ls -a'
+alias ll='ls -l'
+
+direnv hook fish | source
+source (rbenv init - | psub)
+source (nodenv init - | source)
+
+set -x HISTSIZE 100000
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOROOT/bin
+set -x PATH $PATH $HOME/.cargo/bin
+set -x PATH $PATH $HOME/repos/flutter/bin
+set -x PATH $HOME/.ndenv/bin $PATH
+set -x PATH (brew --prefix openssl@1.1)/bin $PATH
+set -x PATH (brew --prefix imagemagick@6)/bin $PATH
+set -x PATH (brew --prefix mysql@5.6)/bin $PATH
+set -x PATH $PATH $ANDROID_HOME/tools
+set -x PATH $PATH $ANDROID_HOME/tools/bin
+set -x PATH $PATH $ANDROID_HOME/platform-tools
+set -x FZF_LEGACY_KEYBINDINGS 0
+
+set -x LDFLAGS -L(brew --prefix mysql@5.6)/lib:/usr/local/opt/openssl/lib
+set -x CPPFLAGS -I(brew --prefix mysql@5.6)/include:(brew --prefix openssl)/include
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
+set -x PATH (brew --prefix openssl@1.1)/bin:$PATH
+set -x LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
+
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+set -x EDITOR vim
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/horiesatoru/google-cloud-sdk/path.fish.inc' ]; . '/Users/horiesatoru/google-cloud-sdk/path.fish.inc'; end
